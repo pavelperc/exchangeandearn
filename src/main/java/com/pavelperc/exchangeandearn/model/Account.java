7ec49093e11dd36a270479cf46c642835e8713c6
pavelperc <pavelperc@mail.ru>
2019-01-26 22:10:14 +0300

    Fixed stackoverflow in user.

--- src/main/java/com/pavelperc/exchangeandearn/model/Account.java
+++ src/main/java/com/pavelperc/exchangeandearn/model/Account.java
@@ -1,18 +1,23 @@
 package com.pavelperc.exchangeandearn.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@ToString(exclude = {"user"})
+@EqualsAndHashCode(of = "id")
 public class Account {
-    
     @Id
     @GeneratedValue
     private long id;
     
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToOne
     private User user;
     
