fa96d99b16aa56b15b8f11b6f29fc9b68c3b1df1
pavelperc <pavelperc@mail.ru>
2019-01-27 08:33:57 +0300

    Predict and predict_max untested.

--- src/main/java/com/pavelperc/exchangeandearn/controller/StatisticController.java
+++ src/main/java/com/pavelperc/exchangeandearn/controller/StatisticController.java
@@ -1,13 +1,13 @@
 package com.pavelperc.exchangeandearn.controller;
 
+import com.pavelperc.exchangeandearn.dto.ValuteDto;
 import com.pavelperc.exchangeandearn.model.Account;
 import com.pavelperc.exchangeandearn.model.Exchange;
+import com.pavelperc.exchangeandearn.model.Rate;
 import com.pavelperc.exchangeandearn.model.User;
 import com.pavelperc.exchangeandearn.repo.ExchangeRepo;
 import com.pavelperc.exchangeandearn.repo.UserRepo;
 import com.pavelperc.exchangeandearn.service.AccountService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +35,9 @@ public class StatisticController {
     @Autowired
     UserRepo userRepo;
     
+    @Autowired
+    CBController cbController;
+    
     @GetMapping("profits/{account_id}")
     public Object[] graphicBuilder(
             @PathVariable("account_id") Account accForeign,
@@ -54,15 +57,44 @@ public class StatisticController {
         return new Object[] {profits, dates};
     }
 
-
-}
-
-/**
- * class function special for front end
- */
-@Data
-@AllArgsConstructor
-class Func {
-    private double[] xArray;
-    private double[] yArray;
+    
+    @GetMapping("predict/{acc_rub_id}/{acc_foreign_id}/{money}")
+    public Double predict(
+            @PathVariable("acc_rub_id") Account accRub,
+            @PathVariable("acc_foreign_id") Account accForeign,
+            @PathVariable("money") Double money,
+            Principal principal
+    ) {
+        User currentUser = userRepo.findByLogin(principal.getName()).get();
+    
+        String currStr = accForeign.getCurrency().getName();
+        Rate rate = getLastRate(currStr);
+        
+        return accountService.getProfit(accRub, accForeign,  rate, money, false);
+    }
+    
+    @GetMapping("predict_max/{acc_rub_id}/{acc_foreign_id}")
+    public Double predict(
+            @PathVariable("acc_rub_id") Account accRub,
+            @PathVariable("acc_foreign_id") Account accForeign,
+            Principal principal
+    ) {
+        User currentUser = userRepo.findByLogin(principal.getName()).get();
+        
+    
+        List<Exchange> history = exchangeRepo.findAllByAccForeign_Id(accForeign.getId());
+        
+        double sum = 0;
+        for (Exchange exchange : history) {
+            sum += exchange.getAddedMoney();
+        }
+        return sum;
+    }
+    
+    Rate getLastRate(String currStr) {
+        ValuteDto dto = cbController.allCurrentCourse().getValutes().stream()
+                .filter(valuteDto -> valuteDto.getCharCode().equals(currStr))
+                .findFirst().get();
+        return new Rate(dto.getValue());
+    }
 }
